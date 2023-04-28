5db3cab68a0e147e12bc527bc3c5472c22a9d8a3
Márcio Reis <122680054+dev-marcioreis@users.noreply.github.com>
2023-04-28 13:47:15 -0300

    add weight

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,53 @@
 <script setup>
 
+  import { ref, shallowRef, computed, watch, nextTick } from 'vue'
+  import { Chart } from 'chart.js';
+  
+  const weights = ref([])
+  const weightChartElement = ref(null)
+  const weightChart = shallowRef(null)
+  const weightInput = ref(60.0)
+
+  const currentWeight = computed(() => {
+    return weights.value.sort((a, b) => b.date - a.date)[0] || { weight: 0 }
+  })
+
+  const addWeight = () => {
+    weights.value.push({
+      weight: weightInput.value,
+      date: new Date().getTime()
+    })
+  }
+
 </script>
 
 <template>
-  <h1>Hello, World!</h1>
+  <main>
+    <h1>Controle de peso</h1>
+    <div class="current">
+      <span>{{ currentWeight.weight }}</span>
+      <small>Peso atual (kg)</small>
+    </div>
+    <form @submit.prevent="addWeight">
+      <input type="number" step="0.1" v-model="weightInput" />
+      <input type="submit" value="Adicionar" />
+    </form>
+    <div v-if="weights && weights.length > 0">
+      <h2>Últimos 7 dias</h2>
+      <div class="canvas-box">
+        <canvas ref="weightChartElement"></canvas>
+      </div>
+      <div class="weight-history">
+        <h2>Hitórico</h2>
+        <ul>
+          <li v-for="weight in weights">
+            <span>{{ weight.weight }}kg</span>
+            <small>{{ new Date(weight.date).toLocaleDateString() }}</small>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </main>
 </template>
 
 <style>
